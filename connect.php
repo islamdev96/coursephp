7a9717d84528950222daa508725f9dc3b094b878
islamdev96 <islamsayedglab@gmail.com>
2023-04-29 12:23:33 +0200

    ss

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=noteapp";
-$user = "root";
-$pass = "";
+$dsn = "mysql:host=localhost;dbname=id20676037_noteapp";
+$user = "id20676037_islamsayed";
+$pass = "253*Islamsayed";
 $option = array(
    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
